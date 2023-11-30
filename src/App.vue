b65581a446dcf71c0a49fb0d37dff86b4b1c7f52
CodeSandbox <support@codesandbox.io>
2023-11-30 20:05:00 +0000

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,76 @@
-<script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="header">
+    <p>Transit Montreal</p>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div>
+    <div v-if="error" class="error">{{ error }}</div>
+    <div v-else>
+      <div ref="map" class="map"></div>
+    </div>
+  </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+import axios from "axios";
+import mapboxgl from "mapbox-gl";
+
+export default {
+  data() {
+    return {
+      error: null,
+      gtfsData: null,
+    };
+  },
+  mounted() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData() {
+      const apiKey = "l7130aafef7c6e481e9aa9fc1c412dd5c9"; // Replace with your actual API key
+      const apiUrl = `https://api.stm.info/pub/od/gtfs-rt/ic/v2?key=${apiKey}`;
+
+      axios
+        .get(apiUrl, { withCredentials: false })
+        .then((response) => {
+          this.gtfsData = response.data;
+
+          // Initialize Mapbox
+          mapboxgl.accessToken =
+            "pk.eyJ1Ijoiam9leWNyZWF0b3IiLCJhIjoiY2xwbGR4bmNqMDNmcDJqcGV1OXY4Z2hteiJ9.yMegQFr9KWFFEsgmdbFRyg";
+          const map = new mapboxgl.Map({
+            container: this.$refs.map,
+            style: "mapbox://styles/mapbox/dark-v10",
+            center: [-73.5673, 45.5017], // Montreal coordinates (adjust as needed)
+            zoom: 12,
+          });
+
+          // Use GTFS-RT data to add markers, layers, etc., on the map
+          this.addMarkers(map);
+        })
+        .catch((error) => {
+          this.error = `Error fetching data: ${error.message}`;
+        });
+    },
+    addMarkers(map) {
+      // Example: Add a marker for each vehicle in the GTFS-RT data
+      if (this.gtfsData) {
+        this.gtfsData.entities.forEach((vehicle) => {
+          const coordinates = [vehicle.longitude, vehicle.latitude];
+          new mapboxgl.Marker().setLngLat(coordinates).addTo(map);
+        });
+      }
+    },
+  },
+};
+</script>
+
+<style>
+#map {
+  width: 100%;
+  height: 100%;
+  position: absolute;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.error {
+  color: red;
 }
 </style>
