ba702ee1254ed4db4bb44b672f1c3a17022d8b36
drjoeycadieux <instructor.joeycadieux@gmail.com>
2023-12-04 15:38:35 -0500

    inm.

--- src/App.vue
+++ src/App.vue
@@ -2,56 +2,59 @@
 <template>
   <div id="app">
     <h1>STM API Messages</h1>
-    <div v-if="messages.length === 0">Loading...</div>
+    <div v-if="loading">Loading...</div>
     <ul v-else>
-      <li v-for="(message, index) in messages" :key="messages">
-        {{ message }}
+      <li v-for="(message, index) in messages">
+        {{ message.id }}
       </li>
     </ul>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
   data() {
     return {
+      loading: true,
       messages: [],
     };
   },
-  // ...
-
+  mounted() {
+    // Fetch data from the API when the component is mounted
+    this.fetchMessages();
+  },
   methods: {
     async fetchMessages() {
       try {
         // Replace 'YOUR_API_KEY' with your actual API key
-        const apiKey = "l769611a1ecd5547a5b6be663d03f3799b";
+        const apiKey = 'l769611a1ecd5547a5b6be663d03f3799b';
 
         const response = await axios.get(
-          "https://api.stm.info/pub/od/i3/v2/messages/etatservice",
+          "https://api.stm.info/pub/od/gtfs-rt/ic/v2/messages/etatservice",
           {
             headers: {
-              apiKey: apiKey,
+              'apiKey': apiKey,
             },
-          },
+          }
         );
 
         // Log the entire response to the console for debugging
-        console.log("API Response:", response);
+        console.log('API Response:', response);
 
-        // Assuming the API response contains an array of messages
-        this.messages = response.data.messages || [];
+        // Assuming the API response contains a 'data' property with a 'messages' array
+        this.messages = response.data.data.messages || [];
+        this.loading = false;
       } catch (error) {
         console.error("Error fetching messages:", error);
 
         // Log the error response to the console for debugging
-        console.log("Error Response:", error.response);
+        console.log('Error Response:', error.response);
+        this.loading = false;
       }
     },
   },
-
-  // ...
 };
 </script>
 
